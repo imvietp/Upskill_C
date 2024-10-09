f3ba52b480579107895be55d197624ce0850cbd7
Phạm Việt <brogun2002@gmail.com>
2024-10-09 18:01:24 +0700

    Review data type

---
+++ data_type.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// There are 3 types of data type:
+// Primitive: int, float, double, char, ...
+// Derived Types: array, pointer, function.
+// User Defined Data Types: struct, enum, union
+
+void primitive_type()
+{
+    int integer_type = 6;
+    float float_type = 6.11;
+    double double_type = 6.132;
+    char char_type = 'd';
+    printf("Integer = %d\n", integer_type);
+    printf("Float = %f\n", float_type);
+    printf("Double = %lf\n", double_type);
+    printf("char_type = %c", char_type);
+}
+
+int main () 
+{
+    primitive_type();
+}
