91950012893158a55fe93e62934975623db9fe9f
Phạm Việt <brogun2002@gmail.com>
2024-10-17 16:13:54 +0700

    Add _print_arithmetic_Operator

--- operator.c
+++ operator.c
@@ -25,7 +25,7 @@ uint8_t bit_manipulation(uint8_t number_original, uint8_t number_change)
     return number_change;
 }
 
-int main()
+void _print_bit_manipulation()
 {
     uint8_t number_o;
     uint8_t number_c;
@@ -34,4 +34,36 @@ int main()
     // uint8_t result = bit_manipulation(&number_one, &number_two);
     uint8_t result = bit_manipulation(number_o, number_c);
     printf("The number after change: %hhu", result);
+}
+
+int _arithmetic_Operator(int first_number, int second_number, int *sum, int *minus, float *time, float *division)
+{
+    *sum         = first_number + second_number;
+    *minus       = first_number - second_number;
+    *time        = first_number * second_number;
+    *division    = first_number / second_number;
+}
+
+void _print_arithmetic_Operator()
+{
+    int a, b;
+    printf("Type number a and b: ");
+    scanf("%d%d", &a, &b);
+
+    int sum, minus;
+    float time, division;
+
+    _arithmetic_Operator(a, b, &sum, &minus, &time, &division);
+
+    printf("Sum      = %d\n", sum);
+    printf("Minus    = %d\n", minus);
+    printf("Time     = %f\n", time);
+    printf("Division = %f\n", division);
+
+}
+
+
+int main()
+{
+    _print_arithmetic_Operator();
 }
